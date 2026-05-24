186dbe70f3eb16b08782e0caa3fde2ac18b73c8e
Mohammed EYAD <mayad5405@gmail.com>
2026-05-24 15:19:34 +0300

    fix shelves routes

--- src/routes/shelves.js
+++ src/routes/shelves.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllShelves } = require('../controllers/shelfController');
+const { getAllShelves, addShelf, deleteShelf } = require('../controllers/shelfController');
 
 router.get('/', getAllShelves);
+router.post('/', addShelf);
+router.delete('/:id', deleteShelf);
 
 module.exports = router;
