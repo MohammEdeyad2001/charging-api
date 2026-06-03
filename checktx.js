088c6fd9d3bbc24d3bfeedccedf61f6cc24e9319
Mohammed EYAD <mayad5405@gmail.com>
2026-06-03 16:49:36 +0300

    cleanup temp files, use shelf_number in transactions, secure gitignore

--- checktx.js
+++
@@ -1,8 +0,0 @@
-﻿const {Pool} = require('pg');
-const p = new Pool({
-  connectionString: 'postgresql://postgres:nNTldTMyEVzaEwOcsoxLmFaXQodAJbLI@trolley.proxy.rlwy.net:49829/railway',
-  ssl: { rejectUnauthorized: false }
-});
-p.query("SELECT column_name FROM information_schema.columns WHERE table_name='transaction'")
-  .then(r => { console.log(r.rows.map(x=>x.column_name)); p.end(); })
-  .catch(e => { console.error('ERROR:', e.message); p.end(); });
--- delc.js
+++
@@ -1,3 +0,0 @@
-﻿const {Pool} = require('pg');
-const p = new Pool({connectionString:'postgresql://postgres:nNTldTMyEVzaEwOcsoxLmFaXQodAJbLI@trolley.proxy.rlwy.net:49829/railway',ssl:{rejectUnauthorized:false}});
-p.query("DELETE FROM customer WHERE id=7").then(()=>{console.log("deleted");p.end();}).catch(e=>{console.error(e.message);p.end();});
